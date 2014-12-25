ce2cea6976220157b7f48b521b9a7f27ca042850
Yohanna Gadelrab <Yohanna@users.noreply.github.com>
2014-12-25 03:43:25 -0500

    Added Transposition cipher.

--- PyCrypt.py
+++ PyCrypt.py
@@ -1,5 +1,6 @@
 import string
 import PyErrors
+import math
 
 # init() # Need to be enabled when running on cmd
 
@@ -67,4 +68,67 @@ def rot13(message, mode='encrypt', scope='all', custom=''):
 
 def reverse(text):
     return text[::-1]  # Extended Slices
+
+
+class Transposition:
+    @staticmethod
+    def encrypt(message, key):
+
+        if key > len(message) / 2:
+            raise PyErrors.WeakKeyError(key, "The key used can't be larger than half the message length.")
+
+        # Each string in cipher_text represents a column in the grid.
+        # The number of columns in the grid equals to the key.
+        columns = key
+
+        cipher_text = [''] * columns
+
+        # Loop through each column in cipher_text.
+        for col in range(columns):
+            pointer = col
+
+            # Keep looping until pointer goes past the length of the message.
+            while pointer < len(message):
+                # Place the character at pointer in message at the end of the
+                # current column in the cipher_text list.
+                cipher_text[col] += message[pointer]
+
+                # move pointer to point to next character in the message.
+                pointer += columns
+
+        # Convert the cipher_text list into a single string value and return it.
+        return ''.join(cipher_text)
+
+    @staticmethod
+    def decrypt(message, key):
+
+        # The transposition decrypt function will simulate the "columns" and
+        # "rows" of the grid that the plain_text is written on by using a list
+        # of strings.
+
+        # The number of columns and rows in our transposition grid:
+        columns = math.ceil(len(message) / key)
+        rows = key
+        # The number of "shaded boxes" in the last "column" of the grid:
+        shaded_boxes = (columns * rows) - len(message)
+
+        # Each string in plain_text represents a column in the grid.
+        plain_text = [''] * columns
+
+        # The col and row variables point to where in the grid the next
+        # character in the encrypted message will go.
+        col = 0
+        row = 0
+
+        for symbol in message:
+            plain_text[col] += symbol
+            col += 1  # point to next column
+
+            # If there are no more columns OR we're at a shaded box, go back to
+            # the first column and the next row.
+            if (col == columns) or (col == columns - 1 and row >= rows - shaded_boxes):
+                col = 0
+                row += 1
+
+        return ''.join(plain_text)
 
